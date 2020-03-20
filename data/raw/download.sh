603ce6ae12d3878f14a99f236669092e6b2a2f89
rejasupotaro <takiguchi0817@gmail.com>
2020-03-20 16:36:43 +0000

    Keep empty data directories

---
+++ data/raw/download.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Compensation
+# Download file before executing from http://appsso.eurostat.ec.europa.eu/nui/setupDownloads.do
+unzip nama_10_lp_ulc.zip
+
+# Education
+# https://ec.europa.eu/eurostat/web/products-datasets/-/trng_lfs_02
+curl -OL https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?file=data/trng_lfs_02.tsv.gz
+gunzip trng_lfs_02.tsv.gz
+
+# Population (number)
+# https://ec.europa.eu/eurostat/web/products-datasets/-/tps00001
+curl -OL https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?file=data/tps00001.tsv.gz
+gunzip tps00001.tsv.gz
+
+# Gross domestic expenditure
+# https://ec.europa.eu/eurostat/web/products-datasets/-/t2020_20
+curl -OL https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?file=data/t2020_20.tsv.gz
+gunzip t2020_20.tsv.gz
+
+# Total R&D personnel by sectors of performance, occupation and sex
+curl -OL https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?file=data/rd_p_persocc.tsv.gz
+gunzip rd_p_persocc.tsv.gz
+
+# R&D expenditure by source of funds (percentage)
+# https://ec.europa.eu/eurostat/web/products-datasets/-/tsc00031
+#curl -OL https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?file=data/tsc00031.tsv.gz
+#gunzip tsc00031.tsv.gz
+
+
+# Population by age group (percentage)
+# https://ec.europa.eu/eurostat/web/products-datasets/-/tps00010
+#curl -OL https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?file=data/tps00010.tsv.gz
+#gunzip tps00010.tsv.gz

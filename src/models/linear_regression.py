ffaebf9fa5891f8466471fc576e1285bd18e1748
rejasupotaro <takiguchi0817@gmail.com>
2020-03-29 16:32:20 +0100

    Show correlation in more detail

--- src/models/linear_regression.py
+++ src/models/linear_regression.py
@@ -6,7 +6,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
-from src.features.features import columns_to_fit
+from ..features.features import columns_to_fit
 
 
 def train():
